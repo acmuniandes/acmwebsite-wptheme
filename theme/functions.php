7ebc07bdd7b5ef5bc67db8891e99830c8e29c7cb
JALopezSilva <jalopezsilva@gmail.com>
2012-09-20 00:12:16 -0400

    Pagination working correctly for categories. Added function for modifying main wordpress query for each category in functions.php

--- theme/functions.php
+++ theme/functions.php
@@ -7,21 +7,40 @@
  */
 
 
-add_action('init','acmtheme_setup');
 
+/**
+ * Function to setup the main features of the theme.
+ */
 function acmtheme_setup()
 {
-
 	/**
 	 * Registers the main navegation menu.
 	 */
 	register_nav_menus(
 		array('nav-menu' => _('Navegation Menu'))
 	);
-
 }
 
+/*
+ * Modifies the main query for each specific template.
+ */
+function modify_query($query)
+{
+	if($query->is_main_query())
+	{
+		//For news
+		if(is_category('news'))
+		{
+			 $query->set('posts_per_page','3');
+		//For activities	 
+		}else if(is_category('activities'))
+		{
+			 $query->set('posts_per_page','2');
 
+		}
+	}
+}
 
-
+add_action('init','acmtheme_setup');
+add_action('pre_get_posts', 'modify_query');
 ?>
