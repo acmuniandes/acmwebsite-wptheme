5420a438fb79661b3ceb02592b31e96f0b6ce3d4
JALopezSilva <jalopezsilva@gmail.com>
2012-09-24 00:31:47 -0400

    Working category-members.php

--- theme/category-members.php
+++ theme/category-members.php
@@ -1,31 +1,43 @@
 <?php get_header(); ?>
-	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-	 <div class="post">
-	
-	 <!-- Display the Title as a link to the Post's permalink. -->
-	 <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-	
-	 <!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-	 <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-	
-	 <!-- Display the Post's Content in a div box. -->
-	 <div class="entry">
-	   <?php the_content(); ?>
-	 </div>
-	
-	 <!-- Display a comma separated list of the Post's Categories. -->
-	 <p class="postmetadata">Posted in <?php the_category(', '); ?></p>
-	 </div> <!-- closes the first div box -->
-	
-
-	<?php endwhile; ?>
-	<div class="navigation">
- 	<div class="alignleft"><?php previous_posts_link('&laquo; Previous Entries') ?></div>
- 	<div class="alignright"><?php next_posts_link('Next Entries &raquo;','') ?></div>
- 	</div>
-		
-	<?php else:?>
-	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-	<?php endif; ?>
+	<div id="main-content" class="container">
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="page-header">
+					<h1><?php single_cat_title(); ?> <small> <?php bloginfo('name'); ?> </small></h1>
+				</div>
+			</div>
+		</div>
+		<!--Grid like images-->
+		<div class="row-fluid posts projects" >
+			<?php 
+			$numpost = 0; 
+			if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				<?php if($numpost % 4 == 0) : ?>
+					<div class="row-fluid">
+				<?php endif; ?>
+						<div class="span3 photo" >
+							<a class="photoPicture thumbnail" href="#"><img src="http://placehold.it/250x250" alt="" /></a>
+							<div class="photoContent" style="display:none;height: 100%;width: 100%;border: 1px solid lightgrey;">
+								<?php the_content(); ?>
+							</div>
+						</div>
+				<?php if($numpost % 4 == 3) : ?>
+					</div>
+				<?php endif; ?>
+				<?php $numposts++; ?>
+			<?php endwhile; else:?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+		</div>
+		<!--Pagination business-->
+		<div class="row-fluid">
+			<div class="span12">
+				<div class="pagination">
+					<ul>
+						<?php list_pagination_links(); ?>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php get_footer(); ?>
