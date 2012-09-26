3962fbbffb3da6837d9fcc419e36fa36993598fc
Juan Tejada <juans.tejada@gmail.com>
2012-09-26 16:11:28 -0500

    Fixes on contact form plus some minor tweaks

--- theme/front-page.php
+++ theme/front-page.php
@@ -2,11 +2,11 @@
 
 		<span></span><div name="outer" class="outer">
       		<span></span><header id="herodiv" class="hero-unit inner">
-        		<h1>Welcome to ACM Uniandes!</h1>
+        		<h1>Bienvenidos a ACM Uniandes!</h1>
         		<div id="description">
-	        		<p>The ACM Student Chapter <a href="http://uniandes.edu.co">@Universidad de los Andes</a> is a group of motivated students passionate about computer science and passionate about experimenting, learning and building awesome stuff for our community.</p>
+	        		<p>El Capítulo Estudiantil ACM <a href="http://sistemas.uniandes.edu.co">@Departamento ISIS, </a><a href="http://uniandes.edu.co">Universidad de los Andes</a> es un grupo de estudiantes motivados y apasionados por la computación y por experimentar, aprender y construir cosas increibles para nuestra comunidad.</p>
 	        		<br/>
-	        		<p><a href="<?php echo get_permalink(get_page_by_title('Nosotros')) ?>" class="btn btn-primary btn-large">Learn more »</a></p>
+	        		<p><a href="<?php echo get_permalink(get_page_by_title('Nosotros')) ?>" class="btn btn-primary btn-large">Aprende más »</a></p>
         		</div>
       		</header>
     	</div>
