1b3d372512acbe198f7fa13077dfa47e21d06c71
Juan Tejada <juans.tejada@gmail.com>
2012-10-11 22:52:43 -0500

    Added analytics to header page

--- theme/header.php
+++ theme/header.php
@@ -12,6 +12,19 @@
 		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		<?php wp_head();?>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-30945558-2']);
+		  _gaq.push(['_trackPageview']);
+		
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		
+		</script>
 	</head>
 	<body <?php body_class();?>>
 		<div id="navbar" class="navbar navbar-fixed-bottom">
